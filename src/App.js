5c7869a223fda4a8ed0d4f255c8dc68be934c2a0
jamesncox <jamesncox@yahoo.com>
2021-05-13 11:36:49 -0500

    Update App.js with memes/fetch from CodeSandbox

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import { useState } from "react";
+
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+export default function App() {
+  const [memes, setMemes] = useState([]);
+  const BASE_URL = "https://api.imgflip.com/get_memes";
+
+  const getMemes = () => {
+    return async () => {
+      try {
+        const res = await fetch(BASE_URL);
+        if (!res.ok) {
+          throw res;
+        }
+        const memeData = await res.json();
+        setMemes(memeData.data.memes);
+      } catch (err) {
+        alert("Failed to load memes");
+      }
+    };
+  };
+
+  const shuffledMemes = shuffle(memes);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>HTML Lazy Loading</h1>
+      <button onClick={getMemes()}>Get Memes</button>
+      {memes.length > 0 &&
+        shuffledMemes.map((meme) => (
+          <div key={meme.id}>
+            <p>{meme.name}</p>
+            <img src={meme.url} alt={meme.name} loading="lazy" />
+          </div>
+        ))}
     </div>
   );
 }
-
-export default App;
